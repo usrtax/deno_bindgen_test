3f5cd029595da3043dc0bb59cc6497f7d08dc6bf
any <any@user.tax>
2022-09-29 18:41:43 +0800

    ♨

--- dist.sh
+++ dist.sh
@@ -4,4 +4,8 @@ DIR=$(dirname $(realpath "$0"))
 cd $DIR
 set -ex
 
+if ! [ -x "$(command -v deno_bindgen)" ]; then
+deno install -Afq -n deno_bindgen https://deno.land/x/deno_bindgen/cli.ts
+fi
+
 deno_bindgen --release
